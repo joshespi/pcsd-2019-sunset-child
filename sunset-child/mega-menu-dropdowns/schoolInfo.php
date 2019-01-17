b74e8c7e4f363fe6b5f4ed80b790e7ac2e5ea2a0
Josh Espinoza <joshespi@gmail.com>
2019-01-17 09:16:38 -0700

    initial Upload

---
+++ sunset-child/mega-menu-dropdowns/schoolInfo.php
@@ -0,0 +1,37 @@
+<div class="dropDownColumn">
+	<h3><img src="https://sandbox.provo.edu/sunset/wp-content/themes/pcsd-2019-theme/assets/icons/calendars.svg" alt="" />Calendars</h3>
+		<ul>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/all-events-calendar/">All Events Calendar</a></li>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/calendar/">Sunset View Calendar</a></li>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/district-school-year-calendar/">School Year Calendar</a></li>
+		</ul>
+	<h3><img src="https://sandbox.provo.edu/sunset/wp-content/themes/pcsd-2019-theme/assets/icons/parents.svg" alt="" />Parent Resources</h3>
+		<ul>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/study-at-home/">Study At Home</a></li>
+			<li class="ext"><a href="https://grades.provo.edu/public/">Grades & Attendance (PowerSchool)</a></li>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/faculty-staff/teachers-directory/">Classrooms</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://sandbox.provo.edu/sunset/wp-content/themes/pcsd-2019-theme/assets/icons/school-info.svg" alt="" />About Sunset View</h3>
+		<ul>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/school-profile/">School Profile</a></li>
+			<li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/contact-our-school/">Contact Our School</a></li>
+            <li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/school-hours/">Hours of Operation &amp; Daily Schedule</a></li>
+            <li class="pdf"><a href="https://sandbox.provo.edu/sunset/wp-content/uploads/2019/01/01152019-SunsetView-AchievmentData.pdf">Achievement Data</a></li>
+            <li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/community-council/">Community Council</a></li>
+            <li class="int"><a href="https://provo.edu/transportation/find-your-bus-route/">Bus Routes</a></li>
+            <li class="int"><a href="https://sandbox.provo.edu/sunset/school-information/parent-teacher-association/">PTA</a></li>
+		</ul>
+</div>
+<div class="dropDownColumn">
+	<h3><img src="https://sandbox.provo.edu/sunset/wp-content/themes/pcsd-2019-theme/assets/icons/school-meals.svg" alt="" />School Meals</h3>
+		<ul>
+			<li class="ext"><a href="https://www.myschoolbucks.com/ver2/login/getmain?requestAction=home">Pay for School Meals</a></li>
+            <li class="ext"><a href="https://www.schoollunchapp.com/Terms.aspx?host=provo.schoollunchapp.com&apply=1">Apply for Free and Reduced Meals</a></li>
+            <li class="ext"><a href="http://itsmealsprovo.com/?page=menus">School Menus</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/special-diet-accommodations/">Special Diet Accommodations</a></li>
+            <li class="int"><a href="https://provo.edu/child-nutrition/">Provo Child Nutrition Website</a></li>
+		</ul>
+	
+</div>
---
+++ sunset-child/mega-menu-dropdowns/staff.php
@@ -0,0 +1,8 @@
+<div class="dropDownColumn">
+						<ul>
+							<li class="int"><a href="https://sandbox.provo.edu/sunset/main-office-administration/">Main Office &amp; Administration</a></li>
+							<li class="int"><a href="https://sandbox.provo.edu/sunset/faculty-staff/teachers-directory/">Teacher Directory</a></li>
+							
+							<li class="int"><a href="https://sandbox.provo.edu/sunset/faculty-staff/education-support-professionals/">Education Support Professionals</a></li>
+						</ul>
+				</div>
