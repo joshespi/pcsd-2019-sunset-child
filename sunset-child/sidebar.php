a5d088ed97330fa60430016384762f739bf5ac25
Josh Espinoza <joshespi@gmial.com>
2019-09-05 10:40:05 -0600

    Added new SafeUT app link to all school sidebars

--- sunset-child/sidebar.php
+++ sunset-child/sidebar.php
@@ -68,6 +68,6 @@
 					</li>
 				</ul>
 				<a href="https://www.peachjar.com/index.php?region=33079&a=28&b=138"><img src="https://sunset.provo.edu/wp-content/uploads/2019/05/button-orange-eflyers_202x46.png" alt="Link to PeachJar Fliers"></a>
-				<a href="https://healthcare.utah.edu/uni/safe-ut/"><img src="https://sunset.provo.edu/wp-content/uploads/2019/03/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
+				<a href="https://safeut.med.utah.edu/"><img src="https://sunset.provo.edu/wp-content/uploads/2019/03/safeUTcrisisline.jpg" alt="Link to SafeUT information"></a>
 			</section>
 		</aside>
