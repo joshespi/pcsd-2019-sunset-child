15eb181e6e71754e87f0f1e82cb4ed4e6d4951c9
Josh Espinoza <joshespi@gmial.com>
2019-10-07 08:36:28 -0600

    Updated title I menu with updated PDFs

--- sunset-child/mega-menu-dropdowns/titlei.php
+++ sunset-child/mega-menu-dropdowns/titlei.php
@@ -4,7 +4,9 @@
 							<li class="int"><a href="https://sunset.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
 							<li class="int"><a href="https://sunset.provo.edu/title-i/patrons-of-title-1-schools-have-the-right-to-know/">Patrons Right to Know</a></li>
 							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/01/SchoolSummary-2016-2017.pdf">School Report Card</a></li>
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/01/SSV-FY19-School-Improvement-Plan.pdf">School Improvement Plan</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-school-improvement-plan-title-i-fy20.pdf">2019/2020 School Improvement Plan</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-title-i-annual-meeting-fy20.pdf">2019/2020 Annual Title I Meeting</a></li>
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-needs-assessment-title-i-fy20.pdf">2019/2020 School Needs Assessment</a></li>
 						</ul>
 						<p>Title I Coordinator: Lance Vandermark (LanceV@provo.edu)</p>
 				</div>
@@ -26,7 +28,7 @@
 						<ul>
 							<li class="int"><a href="https://provo.edu/preschool/title-i-preschool-registration/">Preschool Screener Registration</a></li>						
 							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/01/Sunset-20172018-SAGE-Results.pdf">SAGE Report Card</a></li>
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/01/38124EE6-PROVO-DISTRICT-SUNSET-VIEW-SCHOOL-2017-PACE.pdf">PACE Report Card</a></li>
+							
 							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
 						</ul>
 				</div>
