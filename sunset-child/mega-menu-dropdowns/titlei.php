971398e6c3721ca6702bc36419441e042331ee04
Josh Espinoza <joshespi@gmial.com>
2020-01-27 09:21:01 -0700

    Updated School Report Card

--- sunset-child/mega-menu-dropdowns/titlei.php
+++ sunset-child/mega-menu-dropdowns/titlei.php
@@ -3,7 +3,7 @@
 						<ul>
 							<li class="int"><a href="https://sunset.provo.edu/title-i/what-is-title-i/">What is Title I?</a></li>
 							<li class="int"><a href="https://sunset.provo.edu/title-i/patrons-of-title-1-schools-have-the-right-to-know/">Patrons Right to Know</a></li>
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/01/SchoolSummary-2016-2017.pdf">School Report Card</a></li>
+							
 							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-school-improvement-plan-title-i-fy20.pdf">2019/2020 School Improvement Plan</a></li>
 							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-title-i-annual-meeting-fy20.pdf">2019/2020 Annual Title I Meeting</a></li>
 							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/09/sunset-view-needs-assessment-title-i-fy20.pdf">2019/2020 School Needs Assessment</a></li>
@@ -27,8 +27,7 @@
 					<h3><img src="https://sunset.provo.edu/wp-content/themes/pcsd-2019-theme/assets/icons/policy.svg" alt="" />Additional Resources</h3>
 						<ul>
 							<li class="int"><a href="https://provo.edu/preschool/title-i-preschool-registration/">Preschool Screener Registration</a></li>						
-							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2019/01/Sunset-20172018-SAGE-Results.pdf">SAGE Report Card</a></li>
-							
+							<li class="pdf"><a href="https://sunset.provo.edu/wp-content/uploads/2020/01/SV_SchoolReportCard_fy19.pdf">School Report Card</a></li>
 							<li class="ext"><a href="https://provo.edu/title-i/">District Title I</a></li>
 						</ul>
 				</div>
