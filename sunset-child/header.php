2456bbbb8f5b8072da49074c983622e0caaa922b
Josh Espinoza <joshespi@gmial.com>
2020-02-26 07:37:21 -0700

    updated school fee information, updated instargram link

--- sunset-child/header.php
+++ sunset-child/header.php
@@ -32,7 +32,7 @@
 			</a>
 				<aside>
 					<ul class="sociallinks">
-						<li><a href="http://www.instagram.com/SVDragons_Provo/"><img src="<?php echo get_template_directory_uri() .'/assets/icons/socialmedia-instagram-white.svg' ?>" alt="Link to Instagram" /></span>
+						<li><a href="https://www.instagram.com/sunsetviewdragons/"><img src="<?php echo get_template_directory_uri() .'/assets/icons/socialmedia-instagram-white.svg' ?>" alt="Link to Instagram" /></span>
 							</a>
 						</li>
 						<li><a href="https://twitter.com/SVDragons_Provo"><img src="<?php echo get_template_directory_uri() .'/assets/icons/socialmedia-twitter-white.svg' ?>" alt="Link to Twitter" /></span>
